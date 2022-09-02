376fa5e9ef06013ca861d35944af1c21fbc2325c
Aeirya <aeirya@gmail.com>
2022-09-02 13:58:22 +0000

    update influx retention policy

--- monitoring/conf/setup_influxdb.sh
+++ monitoring/conf/setup_influxdb.sh
@@ -1,1 +1,1 @@
-influx -execute "CREATE RETENTION POLICY one_day_only ON myorg DURATION 1d REPLICATION 1 DEFAULT"
+influx -execute "CREATE RETENTION POLICY one_day_only ON myorg DURATION 1h REPLICATION 1 DEFAULT"
