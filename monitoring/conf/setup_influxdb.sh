21d43879b7baa006ec0f14602e17267b8fdf7946
Sahel Mesforoush <mesforoshs@gmail.com>
2022-09-06 10:45:55 +0430

    rollups

--- monitoring/conf/setup_influxdb.sh
+++ monitoring/conf/setup_influxdb.sh
@@ -1,1 +1,4 @@
 influx -execute "CREATE RETENTION POLICY one_day_only ON myorg DURATION 1h REPLICATION 1 DEFAULT"
+influx -execute "CREATE CONTINUOUS QUERY counting_course_users ON myorg BEGIN SELECT count(DISTINCT(user_id)) INTO counted_course_users FROM myorg_topic GROUP BY time(2s), course_id END"
+influx -execute "CREATE CONTINUOUS QUERY event_counting ON myorg BEGIN SELECT count(*) INTO counted_events FROM myorg_topic group by time(2s), event_place END"
+influx -execute "CREATE CONTINUOUS QUERY counting_users ON myorg BEGIN SELECT count(DISTINCT(user_id)) INTO counted_users FROM myorg_topic  GROUP BY time(30s) END"
